4f249e220c22b42c75e5f74f723abebb05a0cccf
chrisae9 <chrisae9@gmail.com>
2024-09-27 00:12:21 -0400

    copilot baby!!!

--- custom/plugins/gh/gh.plugin.zsh
+++ custom/plugins/gh/gh.plugin.zsh
@@ -1,5 +1,10 @@
 if [[ -n $commands[gh] ]]; then
     source <(gh completion -s zsh)
+    eval "$(gh copilot alias -- zsh)"
 else
     return 1
 fi
+
+alias ghe="ghce"
+alias ghs="ghcs"
+alias ghc="gh copilot config"
